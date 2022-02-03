01c0095d48cfce763720a164bb2266aed715aa37
Alvin Chou <alvinchou516@hotmail.com>
2022-02-03 10:48:20 -0500

    my first js edit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+// document.querySelector();
+// document.querySelectorAll();
+// document.getElementById();
+// document.getElementsByClassName();
+
+// document.createElement();
+// .innerHTML;
+// .textContent;
+// .appenChild();
+
+// const heading3 = document.querySelector('h3');
+// heading3.innerHTML = "JS <strong>fun</strong>";
+
+const footer = document.querySelector('footer');
+footer.children[0].textContent = "My first JS edit";
