595f8da4f18a8168256e0f468a78692ac850d305
Alvin Chou <alvinchou516@hotmail.com>
2022-02-08 22:25:20 -0500

    put all js script on one page

--- scripts.js
+++ scripts.js
@@ -38,7 +38,7 @@ for (let i = 0; i < aElement.length; i++) {
 
 // const inputMessage = document.querySelector("textarea");
 // console.log(inputMessage.id)
-
+if (document.querySelector('form')) {
 const form = document.querySelector('form');
 form.addEventListener('submit', function(e) {
     e.preventDefault();
@@ -46,10 +46,10 @@ form.addEventListener('submit', function(e) {
     const inputText = document.querySelector("input[type=text]");
     const inputEmail = document.querySelector("input[type=email]");
     const inputMessage = document.querySelector("textarea");
-    console.log(inputText.value, inputEmail.value, inputMessage.value);
+    // console.log(inputText.value, inputEmail.value, inputMessage.value);
 
     if (inputText.value && inputEmail.value && inputMessage.value){
-        console.log(`complete`);
+        // console.log(`complete`);
         addComment(inputText.value, inputMessage.value)
         alert('You have successfully submitted!')
         inputText.value = "";
@@ -175,7 +175,7 @@ function timestamp() {
     return `${daysOfTheWeek[today.getDay()]} ${monthsOfTheYear[today.getMonth()]} ${today.getDate()}${ordinal()}, ${today.getFullYear()} `;
 }
 
-console.log(timestamp());
+// console.log(timestamp());
 
 
 const button = document.querySelector(".button");
@@ -183,3 +183,218 @@ const button = document.querySelector(".button");
 form.addEventListener('submit', function() {
     button.style.background = "limegreen";
 })
+}
+
+if (!document.querySelector('.blogMain') && !document.querySelector('.contactMain')) {
+const onPageLoad = function() {
+    const welcome = document.createElement('div')
+    Object.assign(welcome.style, {
+        border: "5px solid #065635",
+        backgroundColor: "#e0fddf",
+        width: "80%",
+        position: "fixed",
+        top: "50vh",
+        left: "50vw",
+        transform: "translate(-50% , -50%)",
+        textAlign: "center",
+        padding: "200px 100px"
+    })
+    welcome.innerHTML = "<h2>Welcome to Sanctuary Helpers</h2><p>If you have any questions please use the contact page</p><p>Enjoy your time while browsing withus</p>";
+    document.querySelector('main').append(welcome);
+    document.querySelector('html').addEventListener('click', () => { welcome.style.display = "none"})
+    setTimeout(() =>{welcome.style.display = "none"}, 5000);
+}
+
+onPageLoad();
+}
+
+// console.log("Slide out nav");
+const slideOutNav = document.querySelector('.dropdownMenu');
+document.querySelector('.dropdownMenu').style.display = "none";
+slideOutNav.classList.add('slideOutNav')
+
+document.querySelector('.mobileNav').appendChild(slideOutNav);
+document.querySelector('.slideOutNav').style.display = "block";
+
+slideOutNav.style.height = "100vh";
+slideOutNav.style.width = "100%";
+slideOutNav.style.maxWidth = "500px";
+slideOutNav.style.backgroundColor = "rgba(255, 255, 255)";
+// slideOutNav.style.position = "fixed";
+// slideOutNav.style.top = "0";
+slideOutNav.style.right = "-500px";
+slideOutNav.style.transition = "0.75s";
+
+Object.assign(slideOutNav.style, {
+    position: "fixed",
+    top: "0"
+})
+// console.log(document.querySelector('.slideOutNav').style.position)
+
+
+// console.log(slideOutNav, "here");
+// console.log(slideOutNav.childNodes, "here");
+// console.log(slideOutNav.childNodes[2], "here");
+
+slideOutNav.childNodes[1].style.textAlign = "center";
+
+
+
+
+const buttonContainer = document.createElement('div')
+buttonContainer.classList.add('buttonContainer')
+buttonContainer.innerHTML = '<button> close </button>'
+
+document.querySelector('.slideOutNav').prepend(buttonContainer)
+document.querySelector('.buttonContainer').style.fontFamily = '"Lato", sans - serif;'
+document.querySelector('.buttonContainer').style.fontWeight = "700";
+document.querySelector('.buttonContainer').style.lineHeight = "1.45rem";
+document.querySelector('.buttonContainer').style.textAlign = "right";
+document.querySelector('.buttonContainer').style.color = "fff";
+
+document.querySelector('.buttonContainer').children[0].style.backgroundColor = "rgba(255, 255, 255)";
+
+// console.log(document.querySelector("input[type=checkbox]:checked"))
+
+document.querySelector("input[type = checkbox]").addEventListener('click', function() {
+    // console.log(document.querySelector("input[type=checkbox]"))
+    // console.log(document.querySelector("input[type=checkbox]").checked)
+    // console.log(document.querySelector("input[type=checkbox]:checked"))
+    document.querySelector('.slideOutNav').style.right = "0"
+})
+
+document.querySelector('.buttonContainer').children[0].addEventListener('click', function() {
+    document.querySelector("input[type=checkbox]").checked = "";
+    document.querySelector('.slideOutNav').style.right = "-500px"
+})
+
+if (document.querySelector("input[type = checkbox]").checked == 'true') {
+    document.querySelector('.slideOutNav').style.right = "0"
+    // console.log(document.querySelector('.slideOutNav').style.right)
+}
+
+// next
+if (!document.querySelector('.blogMain') && !document.querySelector('.contactMain')) {
+
+
+const moreSection = document.querySelector('.more');
+const moreSectionWrapper = moreSection.children;
+// console.log(moreSectionWrapper[0])
+const moreContainer = moreSectionWrapper[0].children;
+// console.log(moreContainer[0]);
+
+
+const moreHeader = moreSection.querySelectorAll('h2');
+// console.log(moreHeader)
+
+moreHeader[0].addEventListener('mouseover', function () {
+    moreContainer[0].style.opacity = "0";
+});
+moreHeader[1].addEventListener('mouseover', function () {
+    moreContainer[1].style.animation = "jump 1s linear infinite";
+});
+moreContainer[3].lastElementChild.children[1].addEventListener('mouseout', function () {
+    moreContainer[0].style.opacity = "1";
+    moreContainer[1].style.animation = "";
+});
+document.addEventListener('scroll', function () {
+    moreContainer[2].style.background = randomColor();
+});
+
+function randomColor() {
+    let a = Math.floor(Math.random() * 256) ;
+    let b = Math.floor(Math.random() * 256) ;
+    let g = Math.floor(Math.random() * 256) ;
+    return `rgb(${a}, ${b}, ${g})`;
+}
+
+// console.log(moreContainer[3]);
+
+
+
+const gallerySection = document.querySelector('.gallery')
+// console.log("Gallery Section Start");
+// console.log(gallerySection);
+
+const arrows = document.createElement('div')
+arrows.classList.add('flexContainer')
+arrows.style.justifyContent = "space-evenly"
+
+const previous = document.createElement('p')
+previous.textContent = "previous"
+arrows.appendChild(previous);
+const next = document.createElement('p')
+next.textContent = "next"
+arrows.appendChild(next);
+gallerySection.appendChild(arrows)
+
+const galleryImgContainer = gallerySection.querySelectorAll('.imgContainer')
+// console.log(galleryImgContainer);
+
+galleryImgContainer[0].style.order = "1";
+galleryImgContainer[1].style.order = "2";
+galleryImgContainer[2].style.order = "3";
+focus();
+
+let numberOfClicks = 0;
+
+next.addEventListener('click', function() {
+    // console.log('next')
+    numberOfClicks++;
+    if (numberOfClicks % 3 == 0) {
+        galleryImgContainer[0].style.order = "1";
+        galleryImgContainer[1].style.order = "2";
+        galleryImgContainer[2].style.order = "3";
+    } else if (numberOfClicks % 3 == 1) {
+        galleryImgContainer[0].style.order = "3";
+        galleryImgContainer[1].style.order = "1";
+        galleryImgContainer[2].style.order = "2";
+    } else if (numberOfClicks % 3 == 2) {
+        galleryImgContainer[0].style.order = "2";
+        galleryImgContainer[1].style.order = "3";
+        galleryImgContainer[2].style.order = "1";
+    };
+    focus();
+});
+
+previous.addEventListener('click', function () {
+    // console.log('previous')
+    // console.log(numberOfClicks);
+    if (numberOfClicks == 0) {
+        numberOfClicks = 3;
+    }
+    numberOfClicks--;
+    if (numberOfClicks % 3 == 0) {
+        galleryImgContainer[0].style.order = "1";
+        galleryImgContainer[1].style.order = "2";
+        galleryImgContainer[2].style.order = "3";
+    } else if (numberOfClicks % 3 == 1) {
+        galleryImgContainer[0].style.order = "3";
+        galleryImgContainer[1].style.order = "1";
+        galleryImgContainer[2].style.order = "2";
+    } else if (numberOfClicks % 3 == 2) {
+        galleryImgContainer[0].style.order = "2";
+        galleryImgContainer[1].style.order = "3";
+        galleryImgContainer[2].style.order = "1";
+    };
+    focus();
+});
+
+function focus() {
+    if (galleryImgContainer[1].style.order == 2) {
+        galleryImgContainer[1].style.opacity = "1";
+        galleryImgContainer[0].style.opacity = "0.2";
+        galleryImgContainer[2].style.opacity = "0.2";
+    } else if (galleryImgContainer[2].style.order == 2) {
+        galleryImgContainer[2].style.opacity = "1";
+        galleryImgContainer[1].style.opacity = "0.2";
+        galleryImgContainer[0].style.opacity = "0.2";
+    } else if (galleryImgContainer[0].style.order == 2) {
+        galleryImgContainer[0].style.opacity = "1";
+        galleryImgContainer[1].style.opacity = "0.2";
+        galleryImgContainer[2].style.opacity = "0.2";
+    }
+}
+
+    
+}
--- scriptsIndex.js
+++ scriptsIndex.js
@@ -20,16 +20,16 @@
 //     galleryImg[1].style.animation = "";
 // });
 
-
+if (document.querySelector('.blogMain') && document.querySelector('.contactMain')) {
 const moreSection = document.querySelector('.more');
 const moreSectionWrapper = moreSection.children;
-console.log(moreSectionWrapper[0])
+// console.log(moreSectionWrapper[0])
 const moreContainer = moreSectionWrapper[0].children;
-console.log(moreContainer[0]);
+// console.log(moreContainer[0]);
 
 
 const moreHeader = moreSection.querySelectorAll('h2');
-console.log(moreHeader)
+// console.log(moreHeader)
 
 moreHeader[0].addEventListener('mouseover', function () {
     moreContainer[0].style.opacity = "0";
@@ -52,13 +52,13 @@ function randomColor() {
     return `rgb(${a}, ${b}, ${g})`;
 }
 
-console.log(moreContainer[3]);
+// console.log(moreContainer[3]);
 
 
 
 const gallerySection = document.querySelector('.gallery')
-console.log("Gallery Section Start");
-console.log(gallerySection);
+// console.log("Gallery Section Start");
+// console.log(gallerySection);
 
 const arrows = document.createElement('div')
 arrows.classList.add('flexContainer')
@@ -73,7 +73,7 @@ arrows.appendChild(next);
 gallerySection.appendChild(arrows)
 
 const galleryImgContainer = gallerySection.querySelectorAll('.imgContainer')
-console.log(galleryImgContainer);
+// console.log(galleryImgContainer);
 
 galleryImgContainer[0].style.order = "1";
 galleryImgContainer[1].style.order = "2";
@@ -83,7 +83,7 @@ focus();
 let numberOfClicks = 0;
 
 next.addEventListener('click', function() {
-    console.log('next')
+    // console.log('next')
     numberOfClicks++;
     if (numberOfClicks % 3 == 0) {
         galleryImgContainer[0].style.order = "1";
@@ -102,8 +102,8 @@ next.addEventListener('click', function() {
 });
 
 previous.addEventListener('click', function () {
-    console.log('previous')
-    console.log(numberOfClicks);
+    // console.log('previous')
+    // console.log(numberOfClicks);
     if (numberOfClicks == 0) {
         numberOfClicks = 3;
     }
@@ -139,90 +139,92 @@ function focus() {
         galleryImgContainer[2].style.opacity = "0.2";
     }
 }
-
-console.log("Slide out nav");
-const slideOutNav = document.querySelector('.dropdownMenu');
-document.querySelector('.dropdownMenu').style.display = "none";
-slideOutNav.classList.add('slideOutNav')
-
-document.querySelector('.mobileNav').appendChild(slideOutNav);
-document.querySelector('.slideOutNav').style.display = "block";
-
-slideOutNav.style.height = "100vh";
-slideOutNav.style.width = "100%";
-slideOutNav.style.maxWidth = "500px";
-slideOutNav.style.backgroundColor = "rgba(255, 255, 255)";
-// slideOutNav.style.position = "fixed";
-// slideOutNav.style.top = "0";
-slideOutNav.style.right = "-500px";
-slideOutNav.style.transition = "0.75s";
-
-Object.assign(slideOutNav.style, {
-    position: "fixed",
-    top: "0"
-})
-// console.log(document.querySelector('.slideOutNav').style.position)
-
-
-console.log(slideOutNav, "here");
-console.log(slideOutNav.childNodes, "here");
-console.log(slideOutNav.childNodes[2], "here");
-
-slideOutNav.childNodes[1].style.textAlign = "center";
-
-
-
-
-const buttonContainer = document.createElement('div')
-buttonContainer.classList.add('buttonContainer')
-buttonContainer.innerHTML = '<button> close </button>'
-
-document.querySelector('.slideOutNav').prepend(buttonContainer)
-document.querySelector('.buttonContainer').style.fontFamily = '"Lato", sans - serif;'
-document.querySelector('.buttonContainer').style.fontWeight = "700";
-document.querySelector('.buttonContainer').style.lineHeight = "1.45rem";
-document.querySelector('.buttonContainer').style.textAlign = "right";
-document.querySelector('.buttonContainer').style.color = "fff";
-
-document.querySelector('.buttonContainer').children[0].style.backgroundColor = "rgba(255, 255, 255)";
-
-console.log(document.querySelector("input[type=checkbox]:checked"))
-
-document.querySelector("input[type = checkbox]").addEventListener('click', function() {
-    console.log(document.querySelector("input[type=checkbox]"))
-    console.log(document.querySelector("input[type=checkbox]").checked)
-    console.log(document.querySelector("input[type=checkbox]:checked"))
-    document.querySelector('.slideOutNav').style.right = "0"
-})
-
-document.querySelector('.buttonContainer').children[0].addEventListener('click', function() {
-    document.querySelector("input[type=checkbox]").checked = "";
-    document.querySelector('.slideOutNav').style.right = "-500px"
-})
-
-if (document.querySelector("input[type = checkbox]").checked == 'true') {
-    document.querySelector('.slideOutNav').style.right = "0"
-    console.log(document.querySelector('.slideOutNav').style.right)
 }
 
+// // console.log("Slide out nav");
+// const slideOutNav = document.querySelector('.dropdownMenu');
+// document.querySelector('.dropdownMenu').style.display = "none";
+// slideOutNav.classList.add('slideOutNav')
 
-const onPageLoad = function() {
-    const welcome = document.createElement('div')
-    Object.assign(welcome.style, {
-        border: "5px solid #065635",
-        backgroundColor: "#e0fddf",
-        width: "80%",
-        position: "fixed",
-        top: "50vh",
-        left: "50vw",
-        transform: "translate(-50% , -50%)",
-        textAlign: "center",
-        padding: "200px 100px"
-    })
-    welcome.innerHTML = "<h2>Welcome to Sanctuary Helpers</h2><p>If you have any questions please use the contact page</p><p>Enjoy your time while browsing withus</p>";
-    document.querySelector('main').append(welcome);
-    document.querySelector('html').addEventListener('click', () => { welcome.style.display = "none"})
-    setTimeout(() =>{welcome.style.display = "none"}, 5000);
-}
+// document.querySelector('.mobileNav').appendChild(slideOutNav);
+// document.querySelector('.slideOutNav').style.display = "block";
 
-onPageLoad();
+// slideOutNav.style.height = "100vh";
+// slideOutNav.style.width = "100%";
+// slideOutNav.style.maxWidth = "500px";
+// slideOutNav.style.backgroundColor = "rgba(255, 255, 255)";
+// // slideOutNav.style.position = "fixed";
+// // slideOutNav.style.top = "0";
+// slideOutNav.style.right = "-500px";
+// slideOutNav.style.transition = "0.75s";
+
+// Object.assign(slideOutNav.style, {
+//     position: "fixed",
+//     top: "0"
+// })
+// // console.log(document.querySelector('.slideOutNav').style.position)
+
+
+// // console.log(slideOutNav, "here");
+// // console.log(slideOutNav.childNodes, "here");
+// // console.log(slideOutNav.childNodes[2], "here");
+
+// slideOutNav.childNodes[1].style.textAlign = "center";
+
+
+
+
+// const buttonContainer = document.createElement('div')
+// buttonContainer.classList.add('buttonContainer')
+// buttonContainer.innerHTML = '<button> close </button>'
+
+// document.querySelector('.slideOutNav').prepend(buttonContainer)
+// document.querySelector('.buttonContainer').style.fontFamily = '"Lato", sans - serif;'
+// document.querySelector('.buttonContainer').style.fontWeight = "700";
+// document.querySelector('.buttonContainer').style.lineHeight = "1.45rem";
+// document.querySelector('.buttonContainer').style.textAlign = "right";
+// document.querySelector('.buttonContainer').style.color = "fff";
+
+// document.querySelector('.buttonContainer').children[0].style.backgroundColor = "rgba(255, 255, 255)";
+
+// // console.log(document.querySelector("input[type=checkbox]:checked"))
+
+// document.querySelector("input[type = checkbox]").addEventListener('click', function() {
+//     // console.log(document.querySelector("input[type=checkbox]"))
+//     // console.log(document.querySelector("input[type=checkbox]").checked)
+//     // console.log(document.querySelector("input[type=checkbox]:checked"))
+//     document.querySelector('.slideOutNav').style.right = "0"
+// })
+
+// document.querySelector('.buttonContainer').children[0].addEventListener('click', function() {
+//     document.querySelector("input[type=checkbox]").checked = "";
+//     document.querySelector('.slideOutNav').style.right = "-500px"
+// })
+
+// if (document.querySelector("input[type = checkbox]").checked == 'true') {
+//     document.querySelector('.slideOutNav').style.right = "0"
+//     // console.log(document.querySelector('.slideOutNav').style.right)
+// }
+
+// if (document.querySelector('.blogMain') && document.querySelector('.contactMain')) {
+// const onPageLoad = function() {
+//     const welcome = document.createElement('div')
+//     Object.assign(welcome.style, {
+//         border: "5px solid #065635",
+//         backgroundColor: "#e0fddf",
+//         width: "80%",
+//         position: "fixed",
+//         top: "50vh",
+//         left: "50vw",
+//         transform: "translate(-50% , -50%)",
+//         textAlign: "center",
+//         padding: "200px 100px"
+//     })
+//     welcome.innerHTML = "<h2>Welcome to Sanctuary Helpers</h2><p>If you have any questions please use the contact page</p><p>Enjoy your time while browsing withus</p>";
+//     document.querySelector('main').append(welcome);
+//     document.querySelector('html').addEventListener('click', () => { welcome.style.display = "none"})
+//     setTimeout(() =>{welcome.style.display = "none"}, 5000);
+// }
+
+// onPageLoad();
+// }
